84d3d2d40deb4898208b94a4a8b9f92abe849f96
dan <danielkarongoi@gmail.com>
2025-03-10 04:53:27 +0300

    added changes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+// 1. Declare companyName as a constant
+const companyName = "Scuber";
+
+// 2. Declare mostProfitableNeighborhood using let
+let mostProfitableNeighborhood = "Chelsea";
+
+// 3. Declare companyCeo using let
+let companyCeo = "Susan Smith";
